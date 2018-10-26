0cab3f73b59414184a4eb8a5e301f415e9860ba7
thanatchakromsang <thanatchakromsang@gmail.com>
2018-10-27 04:03:44 +0700

    ✨ Add example for new storybook setup

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,14 +1,40 @@
-import { configure } from "@storybook/react";
-import { setAddon, addDecorator } from "@storybook/react";
-import JSXAddon from "storybook-addon-jsx";
-import { withKnobs, select } from "@storybook/addon-knobs/react";
-addDecorator(withKnobs);
-setAddon(JSXAddon);
+import * as React from 'react'
 
-// automatically import all files ending in *.stories.js
-const reqComponents = require.context("../src/components/", true, /.stories.tsx$/);
-function loadStories() {
-  reqComponents.keys().forEach(reqComponents);
+import { configure } from '@storybook/react'
+import { setAddon, addDecorator } from '@storybook/react'
+import JSXAddon from 'storybook-addon-jsx'
+import { withKnobs, select } from '@storybook/addon-knobs/react'
+addDecorator(withKnobs)
+setAddon(JSXAddon)
+
+const withStoryStyles = storyFn => {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        display: 'flex',
+        alignItems: 'center',
+        overflow: 'auto',
+      }}
+    >
+      <div style={{ margin: 'auto', maxHeight: '100%' }}>{storyFn()}</div>
+    </div>
+  )
 }
 
-configure(loadStories, module);
+// automatically import all files ending in *.stories.js
+const reqComponents = require.context(
+  '../src/components',
+  true,
+  /.stories.tsx$/
+)
+function loadStories() {
+  addDecorator(withStoryStyles)
+  reqComponents.keys().forEach(reqComponents)
+}
+
+configure(loadStories, module)
--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import './Button.css'
+
 export interface Props {
   label: string
   onClick: () => void
   disabled?: boolean
 }
+
+const StyledButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
 const noop = () => {} // tslint:disable-line
 export const Button = (props: Props) => {
   const { label, onClick, disabled = false } = props
-  const disabledclass = disabled ? 'Button_disabled' : ''
   return (
-    <div
-      className={`Button ${disabledclass}`}
-      onClick={!disabled ? onClick : noop}
-    >
+    <StyledButton onClick={!disabled ? onClick : noop}>
       <span>{label}</span>
-    </div>
+    </StyledButton>
   )
 }
