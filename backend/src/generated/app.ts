b491b345023a6eb44b880bf51243c005d4cde435
thanatchakromsang <thanatchakromsang@gmail.com>
2018-10-27 20:48:09 +0700

    📦 [BE] Update generated files

--- backend/src/generated/app.ts
+++ backend/src/generated/app.ts
@@ -1,15 +1,23 @@
 import { makeBindingClass, Options } from 'graphql-binding'
 import { GraphQLResolveInfo, GraphQLSchema } from 'graphql'
 import { IResolvers } from 'graphql-tools/dist/Interfaces'
-import schema from  '../typeGenerator'
+import schema from '../typeGenerator'
 
 export interface Query {
-    loggedIn: <T = UserPayload>(args?: {}, info?: GraphQLResolveInfo | string, options?: Options) => Promise<T | null> 
-  }
+  loggedIn: <T = UserPayload>(
+    args?: {},
+    info?: GraphQLResolveInfo | string,
+    options?: Options
+  ) => Promise<T | null>
+}
 
 export interface Mutation {
-    login: <T = AuthPayload>(args: { email: String, password: String }, info?: GraphQLResolveInfo | string, options?: Options) => Promise<T> 
-  }
+  login: <T = AuthPayload>(
+    args: { email: String; password: String },
+    info?: GraphQLResolveInfo | string,
+    options?: Options
+  ) => Promise<T>
+}
 
 export interface Subscription {}
 
@@ -17,25 +25,39 @@ export interface Binding {
   query: Query
   mutation: Mutation
   subscription: Subscription
-  request: <T = any>(query: string, variables?: {[key: string]: any}) => Promise<T>
-  delegate(operation: 'query' | 'mutation', fieldName: string, args: {
-      [key: string]: any;
-  }, infoOrQuery?: GraphQLResolveInfo | string, options?: Options): Promise<any>;
-  delegateSubscription(fieldName: string, args?: {
-      [key: string]: any;
-  }, infoOrQuery?: GraphQLResolveInfo | string, options?: Options): Promise<AsyncIterator<any>>;
-  getAbstractResolvers(filterSchema?: GraphQLSchema | string): IResolvers;
+  request: <T = any>(
+    query: string,
+    variables?: { [key: string]: any }
+  ) => Promise<T>
+  delegate(
+    operation: 'query' | 'mutation',
+    fieldName: string,
+    args: {
+      [key: string]: any
+    },
+    infoOrQuery?: GraphQLResolveInfo | string,
+    options?: Options
+  ): Promise<any>
+  delegateSubscription(
+    fieldName: string,
+    args?: {
+      [key: string]: any
+    },
+    infoOrQuery?: GraphQLResolveInfo | string,
+    options?: Options
+  ): Promise<AsyncIterator<any>>
+  getAbstractResolvers(filterSchema?: GraphQLSchema | string): IResolvers
 }
 
 export interface BindingConstructor<T> {
-  new(...args): T
+  new (...args): T
 }
 
 export const Binding = makeBindingClass<BindingConstructor<Binding>>({ schema })
 
 /**
  * Types
-*/
+ */
 
 export interface AuthPayload {
   token: String
